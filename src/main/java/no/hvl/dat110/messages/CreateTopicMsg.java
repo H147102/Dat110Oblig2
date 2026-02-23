e95529e82e7cba1f54b75d6730e77bd3fa900017
H147102 <147102@stud.hvl.no>
2026-02-23 10:53:44 +0100

    Update CreateTopicMsg.java

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -3,11 +3,27 @@ package no.hvl.dat110.messages;
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
+	private String topic;
 
     public CreateTopicMsg(String user, String topic) {
+		super(MessageType.CREATETOPIC, user);
+		this.topic = topic;
 
-    }
+		
+	}
 
+	public String GetTopic() {
+		return topic;
+	}
+
+	public void SetTopic() {
+		this.topic = topic;
+	}
+
+	@override
+	public String toString() {
+		return "CreateTopicMsg [user= "+ GetUser() + ", topic = "+ topic +"]";
+	}
 	// TODO: 
 	// Implement object variables - a topic is required
 	
