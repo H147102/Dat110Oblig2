620aa3fb8092c177c32e9c2a61ecb394608b47f2
H147102 <147102@stud.hvl.no>
2026-02-23 13:39:34 +0100

    Update SubscribeMsg.java

--- src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
@@ -2,11 +2,26 @@ package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
 
+	String Topic
+
 	// message sent from client to subscribe on a topic 
 
     public SubscribeMsg(String user, String topic) {
-
+		super(MessageType.SUBSCRIBE, user);
+		this.topic = topic;
     }
+
+	public String getTopic(){
+		return topic;
+	}
+
+	public void setTopic(){
+		this.topic = topic;
+	}
+
+	public String toString(){
+		return "Message [topic="+topic+", type="+getType()+", user="+getUser();
+	}
 
 	// TODO:
 	// Implement object variables - a topic is required
