77c20bd9f1742d562f74e23e744650b81e0c1b9b
H147102 <147102@stud.hvl.no>
2026-02-23 11:18:13 +0100

    Update DeleteTopicMsg.java

--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -3,11 +3,14 @@ package no.hvl.dat110.messages;
 public class DeleteTopicMsg extends Message {
 
 	// message sent from client to create topic on the broker
+	String topic;
 
     public DeleteTopicMsg(String user, String topic) {
-
+		super(MessageType.DELETETOPIC, user);
+		this.topic = topic;
     }
 
+	get
 	// TODO:
 	// Implement object variables - a topic is required
 
