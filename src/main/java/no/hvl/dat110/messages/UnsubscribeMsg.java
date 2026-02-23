a63473680235f86b40d6c47eb70bed7be117c180
H147102 <147102@stud.hvl.no>
2026-02-23 13:46:08 +0100

    Update UnsubscribeMsg.java

--- src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -2,11 +2,25 @@ package no.hvl.dat110.messages;
 
 public class UnsubscribeMsg extends Message {
 
+	String topic;
 	// message sent from client to unsubscribe on a topic 
 
     public UnsubscribeMsg(String user, String topic) {
-
+		super(MessageType.UNSUBSCRIBE, user);
+		this.topic = topic;
     }
+
+	public String getTopic(){
+		return topic;
+	}
+
+	public void setTopic(){
+		this.topic = topic;
+	}
+
+	public String toString(){
+		return "UnsubscribeMsg [topic="+topic+", type="+getType()+", user="+getUser();
+	}
 
 	// TODO:
 	// Implement object variables - a topic is required
