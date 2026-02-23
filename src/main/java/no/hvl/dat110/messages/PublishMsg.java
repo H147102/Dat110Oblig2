ac29af64e0013cd62d95f7b389c01e344b0fe64d
H147102 <147102@stud.hvl.no>
2026-02-23 13:53:20 +0100

    Update PublishMsg.java

--- src/main/java/no/hvl/dat110/messages/PublishMsg.java
+++ src/main/java/no/hvl/dat110/messages/PublishMsg.java
@@ -3,11 +3,36 @@ package no.hvl.dat110.messages;
 import no.hvl.dat110.common.TODO;
 
 public class PublishMsg extends Message {
+
+	String topic;
+	String message;
 	
 	// message sent from client to create publish a message on a topic 
 
 	public PublishMsg(String user, String topic, String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message:
+	}
 
+	public String getTopic() {
+		return topic;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setTopic() {
+		this.topic = topic;
+	}
+
+	public void setMessage() {
+		this.message = message;
+	}
+
+	public String toString(){
+		return "publishMsg [topic="+topic+", message="+message+", type="+getType()+", user="+getUser();
 	}
 
 	// TODO:
@@ -15,9 +40,5 @@ public class PublishMsg extends Message {
 
 	// Complete the constructor, get/set-methods, and toString method
 	// as described in the project text
-	
-	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
 	}
 }
